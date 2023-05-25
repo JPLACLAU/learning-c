c81f2a1f760ebe065b5e54e60922278284b8b517
JPLACLAU <laclaujeanpaul@gmail.com>
2023-05-25 13:12:44 -0300

    Ejercicio 1.4

---
+++ Week 1/Project 1/1.5 Character Input and Output/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* copy input to output; 1st version */
+
+int main()
+{
+    int c;
+
+    c= getchar();
+    while (c |= EOF){
+        putchar(c);
+        c = getchar();
+    }
+}

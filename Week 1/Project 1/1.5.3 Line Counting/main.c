cd6b030145e9c911134251994c3fc5273547eead
JPLACLAU <laclaujeanpaul@gmail.com>
2023-05-27 15:59:35 -0300

    Ejercicio 1.5.3; E1-9: salio raro pero OK

--- Week 1/Project 1/1.5.3 Line Counting/main.c
+++ Week 1/Project 1/1.5.3 Line Counting/main.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* count lines in input */
+/* count lines, tabs, and blanks in input */
+/* Exercise 1-9: Write a program to copy its input to its output, replacing each
+string of one or more blanks by a single blank. */
 
 int main()
 {
-    int c, nl;
+    int c, nl, blank, tab, lastChar;
 
     nl = 0;
-    while ((c = getchar()) !=EOF)
-    {
+    blank = 0;
+    tab = 0;
+    lastChar = ' '; //initialize with a blank
 
-        if ( c == '\n')
-        ++nl;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+            ++nl;
+        else if (c == '\t')
+            ++tab;
+
+        else if (c == ' ') {
+            if (lastChar != ' ')
+                putchar(c);
+                ++blank;
+        }
+        else { putchar(c);}
+
+        lastChar = c; // store last character for comparison
 
     }
-    printf("%d\n", nl);
 
+    printf("Lines: %d\n", nl);
+    printf("Tabs: %d\n", tab);
+    printf("Blanks: %d\n", blank);
 
+    return 0;
 }

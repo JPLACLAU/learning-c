7487a946a4e4f72a8d554eb4076a6bd1947cc225
JPLACLAU <laclaujeanpaul@gmail.com>
2023-05-27 02:46:01 -0300

    .

---
+++ Week 1/Project 1/1.5.3 Line Counting/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* count lines in input */
+
+int main()
+{
+    int c, nl;
+
+    nl = 0;
+    while ((c = getchar()) !=EOF)
+    {
+
+        if ( c == '\n')
+        ++nl;
+
+    }
+    printf("%d\n", nl);
+
+
+}
